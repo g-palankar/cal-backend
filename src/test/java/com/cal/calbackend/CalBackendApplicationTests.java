510bfadba2f02f3ba7f492d08c936ed96d8b5248
Ganesh S Palankar <ganeshpalankar01@gmail.com>
2023-11-12 16:17:21 +0530

    Initial Commit
    Added Spring Data JPA, Spring Security

---
+++ src/test/java/com/cal/calbackend/CalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cal.calbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
